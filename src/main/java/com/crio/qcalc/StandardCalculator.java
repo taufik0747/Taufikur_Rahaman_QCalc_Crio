e1ae8c5cde8988a1bfe6165271ef522034d269b5
taufik0747 <rahamantaufik1999@gmail.com>
2023-07-19 20:35:18 +0530

    ME_QCALC_V2_MODULE_IMPLEMENTATION  (Crio.Do)

--- src/main/java/com/crio/qcalc/StandardCalculator.java
+++ src/main/java/com/crio/qcalc/StandardCalculator.java
@@ -9,19 +9,19 @@ public class StandardCalculator {
     public void clearResult(){
         result=0;
     }
-    
-
 
     public void add(int num1,int num2){
         result = num1 + num2;
-        
     }
+
     public void subtract(int num1,int num2){
         result= num1-num2;
     }
+
     public void multiply(int num1,int num2){
         result=num1*num2;
     }
+    
     public void divide(int num1,int num2){
         result= num1/num2;
     }
